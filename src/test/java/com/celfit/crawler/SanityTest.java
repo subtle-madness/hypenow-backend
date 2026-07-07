cd8c47aeb6da1bf6c529d447847487b8a4b46606
woomin <chlwoomin@naver.com>
2026-07-07 19:14:50 +0900

    chore: Spring Boot 4.1 프로젝트 스캐폴딩 (gradle, compose, 설정)

---
+++ src/test/java/com/celfit/crawler/SanityTest.java
@@ -0,0 +1,12 @@
+package com.celfit.crawler;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SanityTest {
+    @Test
+    void 빌드_동작() {
+        assertEquals(2, 1 + 1);
+    }
+}
