3a1ab6d7a2633b2741efc62efae5440770bc82af
woomin <chlwoomin@naver.com>
2026-07-07 20:03:43 +0900

    feat: 액터별 raw 엔티티 (jsonb payload + generated column 읽기)

---
+++ src/main/java/com/celfit/crawler/domain/RawDiscoveryPostRepository.java
@@ -0,0 +1,6 @@
+package com.celfit.crawler.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RawDiscoveryPostRepository extends JpaRepository<RawDiscoveryPost, Long> {
+}
